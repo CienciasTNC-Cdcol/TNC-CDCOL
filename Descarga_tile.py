8902dbea4c5aaaaa13db715f5cc4d9f4aa75fde2
CienciasTNC-Cdcol <42647185+CienciasTNC-Cdcol@users.noreply.github.com>
2018-11-16 08:30:38 -0500

    Update Descarga_tile.py

--- Descarga_tile.py
+++ Descarga_tile.py
@@ -1,1 +1,12 @@
+__autor__ = "TNC - Miguel Angel Canon Ramos"
+__credits__ = ["none"]
+__license__ = "Uso Libre"
+__version__ = "1.0"
+__correo__ = "cienciastnc@gmail.com, miguelca27@gmail.com"
+__status__ = "Version 1.0"
+
+"""
+Codigo para la descarga de imagenes satelitales por "tile" de un grado por un grado en el Cubo de datos Colombia
+"""
+
 output= xarr0
